aa491f9b24ce957eb36654fb17c000cda438a252
mahmoud2391997 <114721167+mahmoud2391997@users.noreply.github.com>
2023-08-13 23:47:10 +0300

    vip

---
+++ Program (2).cs
@@ -0,0 +1,231 @@
+﻿using System;
+    namespace Program1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            // binary operator
+            var amount = Math.Cos(30) + 1;
+            Console.WriteLine(amount);
+            Console.WriteLine("mahmoud");
+            var x = 2;
+            var y = 5;
+            Console.WriteLine($"x + y = {x + y}");
+            Console.WriteLine($"x - y = {x - y}");
+            Console.WriteLine($"x / y = {x / y}");
+            Console.WriteLine($"x * y = {x * y}");
+            Console.WriteLine($"x % y = {x % y}");
+            var z = 8 / 4 / 2; //1
+            var Z = 8 / (4 / 2); //4
+            Console.WriteLine(Z);
+            var s1 = "";
+            var s2 = "";
+            var s3 = s1 = s2 = "mahmoud";
+            Console.WriteLine(s1);
+
+
+            x = x + 10;//12
+            // null operator
+            string S1 = null;
+            S1 = S1 ?? "mahmoud";
+            Console.WriteLine(S1);
+            S1 = S1 ?? "someone else";
+            Console.WriteLine(S1);
+            string S2 = null;
+            var S3 = S2?.ToUpper();
+            Console.WriteLine(S3);
+            // statement and statement blocks
+            Console.WriteLine("hi");
+            {
+                Console.WriteLine("hi");
+                Console.WriteLine("hello");
+
+            }
+            //declaration statement 
+            int X;
+            //Expression statement 
+            //1. change state
+            var name = "mahmoud";
+            name = name + "elsayed";
+            //2.call something that change the state
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            //3. increment and decrement 
+            var totalfriends = 150;
+            ++totalfriends;
+            Console.WriteLine(totalfriends);
+            --totalfriends;
+            Console.WriteLine(totalfriends);
+            var Q = 2;
+            Console.WriteLine(Q++);
+            Console.WriteLine(Q);
+            //5. object instansiation
+            Object o = new object();
+            //if
+            var mark = 90;
+            if (mark >= 85)
+            {
+                Console.WriteLine("Excellent");
+            }
+            mark = 55;
+            if (mark >= 60)
+            {
+                Console.WriteLine("passed");
+            }
+            else if(mark >=55)
+            {
+                Console.WriteLine("you can have a chance in a make up exam");
+            }
+            else
+            {
+                Console.WriteLine("failed");
+            }
+            if (mark >= 60)
+            {
+                if (mark >= 85)
+                {
+                    Console.WriteLine("Excellent");
+                }
+            }
+            //switch
+            var amountEGP = 100;
+            var currType = "USD";
+            var output = 0d;
+
+            var USDtoEGP = 33d;
+           switch(currType)
+            {
+                case "USD":
+                    output = amountEGP / USDtoEGP;
+                    Console.WriteLine(output);
+                    break;
+            }
+            var  NO= 3;
+            switch (NO)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    Console.WriteLine("ODD");
+                    break;
+                case 2:
+                case 4:
+                case 6:
+                case 8:
+                    Console.WriteLine("even");
+                    break;
+            }
+            Object K = "Mahmoud";
+            switch(K)
+            {
+                case int i: Console.WriteLine($"it's int,sqr of{i}={i * i}");
+                    break;
+                case string i: Console.WriteLine($"it's a string, capitalization of {i} = {i.ToUpper()}");
+                    break;     }
+            bool IsVip = true;
+            switch (IsVip)
+            {
+                case bool i when i == true:
+                    Console.WriteLine("yes");
+                    break;
+                case bool i:
+                    Console.WriteLine("no");
+                    break;
+                    
+            }
+            var cardNo = 13;
+            string cardName = cardNo switch
+            {
+                1 => "ACE",
+                13 => "King",
+                12 => "Queen",
+                11 => "Jack",
+                _ => cardNo.ToString()
+
+
+            };
+            Console.WriteLine(cardName);
+            //iteration
+            //while
+            var counter =0;
+
+            while (counter < 10)
+            {
+                Console.Write(counter + " ");
+                counter++;
+            }
+            counter = 0;
+            do
+            {
+                Console.Write(counter + " ");
+                counter++;
+
+            } while (counter < 10);
+            //for
+            
+            for(var count = 0;count < 10; ++count)
+            {
+                Console.Write(count + " ");
+            }
+            //fibonacci{0,1,1,2,3,5,8,13,21,34}
+            for (int count = 0 ,prev =0 , current = 1; count < 10; ++count)
+            {
+                Console.Write(prev + " ");
+                int newFib = prev + current;
+                prev = current;
+                current = newFib;
+            }
+            // infinite loop for(; ; ){}
+            //for each
+            
+            foreach(char c in "full stack developer course")
+            {
+                Console.Write(c+ " ");
+
+            };
+            var arr = new int[] { 1, 2, 3, 4 };
+            foreach(var n in arr)
+            {
+                Console.Write(n + " ");
+            }
+            for (var i = 0;i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            //Jump{break,continue,goto, return}
+            var I = 0;
+            while(I<10)
+            {
+                if (I > 5)
+                    break;
+                Console.Write(I + " ");
+                I++;
+            }
+            for(int i =0; i< 10; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                        Console.Write(i+" ");
+            }
+            var P = 0;
+            start:
+            if(P <= 5)
+            {
+                Console.Write(P + " ");
+                P++;
+                goto start;
+                Console.WriteLine("mahmoud");
+            }
+            var input = .44m;
+            var result = AsPercentage(input);
+            Console.WriteLine(result);
+            static decimal AsPercentage(decimal amount)
+            {
+                return amount * 100;
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿namespace Program;
+﻿//namespace program;
 
-//internal class System
+//internal class system
 //{
-//    private static void Main(string[] args)
+//    private static void main(string[] args)
 //    {
-//        // 1.Declaration [ <DataType> <identifier>;]
+//        // 1.declaration [ <datatype> <identifier>;]
 //        int num;
 //        // 2. assigment [<identifier> = <value>;]
 //        num = 5;
-//        //3. initialization [<Datatype> <identifier> = <initial value>;]
+//        //3. initialization [<datatype> <identifier> = <initial value>;]
 //        int num2 = 5;
 //        //string reference type
 //        string s1;
@@ -16,175 +16,176 @@
 //        string s2 = "mohamed";
 //        // regular concatination (plus sign)
 //        string s3 = s1 + " " + s2;
-//        Console.WriteLine(s3); //mahmoud mohamed
+//        console.writeline(s3); //mahmoud mohamed
 //        string s4 = $"{s1} {s2}";
-//        Console.WriteLine(s4);//mahmoud mohamed
-//        Console.WriteLine($"int: [{int.MinValue} → {int.MaxValue}]");
+//        console.writeline(s4);//mahmoud mohamed
+//        console.writeline($"int: [{int.minvalue} → {int.maxvalue}]");
 //        //var
-//        var S1 = "mahmoud";
+//        var s1 = "mahmoud";
 //        var f = 0f;
 //        var d = 0d;
 //        var m = 0m;
 //        var u = 0u;
 //        var l = 0l;
 //        var ul = 0ul;
-//        int oneMillion = 1_000_000;
+//        int onemillion = 1_000_000;
 //        var result = 200 / 3.0;
 
 //        //dynamic
 //        dynamic x = 9;
 //        x = "abc";
 //        x = 10m;
-//        char letter = 'A';
+//        char letter = 'a';
 //        var id = 1000;
 //        var fname = "mahmoud";
-//        var lname = "A.";
+//        var lname = "a.";
 //        var salary = 1000d;
-//        var gender = 'M';
+//        var gender = 'm';
 //        var address = "ismailia,egypt";
-//        Console.WriteLine("Employee");
-//        Console.WriteLine("--------");
-//        Console.WriteLine($"id: {id}");
-//        Console.WriteLine($"name: {lname}");
-//        Console.WriteLine($"salary: {salary}");
-//        Console.WriteLine($"gender: {gender}");
-//        Console.WriteLine($"address: {address}");
-//        var X = 10;
+//        console.writeline("employee");
+//        console.writeline("--------");
+//        console.writeline($"id: {id}");
+//        console.writeline($"name: {lname}");
+//        console.writeline($"salary: {salary}");
+//        console.writeline($"gender: {gender}");
+//        console.writeline($"address: {address}");
+//        var x = 10;
 //        var y = 10;
-//        // Console.WriteLine(X == y); // is X equal to y? => true
-//        //Expression 1+2+3
-//        var z = X == y;
-//        Console.WriteLine(z);
+//        // console.writeline(x == y); // is x equal to y? => true
+//        //expression 1+2+3
+//        var z = x == y;
+//        console.writeline(z);
 //        var total = 1000;
-//        var vipThreshold = 900;
-//        var isVip = total >= vipThreshold; // > , < , <=
-//        Console.WriteLine(isVip);
-//        isVip = !true;
-//        Console.WriteLine(isVip);
+//        var vipthreshold = 900;
+//        var isvip = total >= vipthreshold; // > , < , <=
+//        console.writeline(isvip);
+//        isvip = !true;
+//        console.writeline(isvip);
 //        // && ||
 //        //logical and &&
-//        Console.WriteLine(true && true);
-//        Console.WriteLine(true && false);
-//        Console.WriteLine(false && true);
-//        Console.WriteLine(false && false);
+//        console.writeline(true && true);
+//        console.writeline(true && false);
+//        console.writeline(false && true);
+//        console.writeline(false && false);
 
 //        //logical or !!
-//        Console.WriteLine(true || true);
-//        Console.WriteLine(true || false);
-//        Console.WriteLine(false || true);
-//        Console.WriteLine(false || false);
+//        console.writeline(true || true);
+//        console.writeline(true || false);
+//        console.writeline(false || true);
+//        console.writeline(false || false);
 
 //        //logical xor
-//        Console.WriteLine(true ^ true);
-//        Console.WriteLine(true ^ false);
-//        Console.WriteLine(false ^ true);
-//        Console.WriteLine(false ^ false);
-//        var isFirstFive = true;
-//        var GPA = 3.5;
-//        var isElegibleToScholarship = GPA >= 3.5 && isFirstFive;
-//        var isFirstFive2 = true;
-//        var GPA2 = 3.4;
-//        var isElegibleToScholarship2 = GPA2 >= 3.5 || isFirstFive2;
-//        Console.WriteLine(isElegibleToScholarship);
-//        Console.WriteLine(isElegibleToScholarship2);
-//        Console.WriteLine(true & true);
-//        Console.WriteLine(true & false);
-//        Console.WriteLine(false & true);
-//        Console.WriteLine(false & false);
-//        bool value = Check() || true;
-//        var A = 1;
-//        var B = 2;
-//        var C = x == y;
-//        Console.WriteLine(C);
+//        console.writeline(true ^ true);
+//        console.writeline(true ^ false);
+//        console.writeline(false ^ true);
+//        console.writeline(false ^ false);
+//        var isfirstfive = true;
+//        var gpa = 3.5;
+//        var iselegibletoscholarship = gpa >= 3.5 && isfirstfive;
+//        var isfirstfive2 = true;
+//        var gpa2 = 3.4;
+//        var iselegibletoscholarship2 = gpa2 >= 3.5 || isfirstfive2;
+//        console.writeline(iselegibletoscholarship);
+//        console.writeline(iselegibletoscholarship2);
+//        console.writeline(true & true);
+//        console.writeline(true & false);
+//        console.writeline(false & true);
+//        console.writeline(false & false);
+//        bool value = check() || true;
+//        var a = 1;
+//        var b = 2;
+//        var c = x == y;
+//        console.writeline(c);
 //        var v1 = "hello";
 //        var v2 = "hello";
 //        var v3 = v1 == v2;
-//        Console.WriteLine(v3);
-//        var totalPurchases = 900;
-//        var VipThreshold = 1000;
-//        var Vip = total >= vipThreshold ? true : false;
-//        var discount = total >= vipThreshold ? "0.05%" : "0.0%";
-//        Console.WriteLine(discount);
+//        console.writeline(v3);
+//        var totalpurchases = 900;
+//        var vipthreshold = 1000;
+//        var vip = total >= vipthreshold ? true : false;
+//        var discount = total >= vipthreshold ? "0.05%" : "0.0%";
+//        console.writeline(discount);
 
 
-//        Console.ReadKey();
-//        static bool Check()
+//        console.readkey();
+//        static bool check()
 //        {
-//            Console.WriteLine("checking...");
+//            console.writeline("checking...");
 //            return true;
 //        }
 //    }
 //}
 
-//Arrays
+////arrays
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        // single dimensional array
-        //declaration
-        string[] friends = new string[5];
-        //2.Accessing element
-        friends[0] = "Ali";
-        friends[1] = "Reem";
-        friends[2] = "Faisal";
-        friends[3] = "ahmed";
-        friends[4] = "Abeer";
-        //accessing elements with another way
-        string[] friends1 = new string[5]
-        {
-            "Ali","Reem","Faisal","ahmed","Abeer"
-        };
-        //accessing elements with another way
-        string[] friends2 =  {
-            "Ali","Reem","Faisal","ahmed","Abeer"
-        };
-        // multi dimensional array {rectangular array}
-        int[,] suduko = {
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 },
-        {1,2,3,4,5,6,7,8,9 }
-        };
-        // jagged array {array inside array}
-        var jagged = new int[][]
-        {
-            new int[] {1,2,3,4},
-            new int[] {1,2},
-            new int[] {3,4,5}
-        };
-        var friends4 = new string[] { "Ali", "Reem" };
-        var first = friends4[0];
-        // indices and ranges
-        var slices = friends[..2];
-        slices.Print();
-        Console.WriteLine(first);
-        Console.ReadKey(); }
+//class program
+//{
+//    static void main(string[] args)
+//    {
+//        // single dimensional array
+//        //declaration
+//        string[] friends = new string[5];
+//        //2.accessing element
+//        friends[0] = "ali";
+//        friends[1] = "reem";
+//        friends[2] = "faisal";
+//        friends[3] = "ahmed";
+//        friends[4] = "abeer";
+//        //accessing elements with another way
+//        string[] friends1 = new string[5]
+//        {
+//            "ali","reem","faisal","ahmed","abeer"
+//        };
+//        //accessing elements with another way
+//        string[] friends2 =  {
+//            "ali","reem","faisal","ahmed","abeer"
+//        };
+//        // multi dimensional array {rectangular array}
+//        int[,] suduko = {
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 },
+//        {1,2,3,4,5,6,7,8,9 }
+//        };
+//        // jagged array {array inside array}
+//        var jagged = new int[][]
+//        {
+//            new int[] {1,2,3,4},
+//            new int[] {1,2},
+//            new int[] {3,4,5}
+//        };
+//        var friends4 = new string[] { "ali", "reem" };
+//        var first = friends4[0];
+//        // indices and ranges
+//        var slices = friends[..2];
+//        slices.print();
+//        console.writeline(first);
+//        console.readkey();
+//    }
 
-    }
+//}
 
-public static class Extensions
-{
-    public static void Print<T>(this T[] source)
-    {
-        if(!source.Any())
-        {
-            Console.WriteLine("{}");
-            return;
-        }
-        Console.WriteLine("{ ");
-        for(var i=0; i<source.Length;i++)
-        {
-            Console.WriteLine($"{source[i]}");
-            Console.WriteLine(i < source.Length - 1 ? ", " : "");
+//public static class extensions
+//{
+//    public static void print<t>(this t[] source)
+//    {
+//        if (!source.any())
+//        {
+//            console.writeline("{}");
+//            return;
+//        }
+//        console.writeline("{ ");
+//        for (var i = 0; i < source.length; i++)
+//        {
+//            console.writeline($"{source[i]}");
+//            console.writeline(i < source.length - 1 ? ", " : "");
 
-        }
-        Console.WriteLine(" }");
-    }
-}
+//        }
+//        console.writeline(" }");
+//    }
+//}
